018a00825a967cb4859f63386074f64b2c2c4518
s-t-a-n <stan@wopwop.cc>
2020-08-27 13:51:01 +0200

    removed banners, formatted cpp/hpp files with #: find . -name "*.*pp" -type f -exec clang-format -i -style=webkit {} \;

--- Libraries/Sensor/Sensor.hpp
+++ Libraries/Sensor/Sensor.hpp
@@ -1,23 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   Sensor.hpp                                         :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: sverschu <sverschu@student.codam.n>          +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2020/08/26 16:49:25 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/08/26 17:47:12 by sverschu      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SENSOR_HPP
-# define SENSOR_HPP
+#define SENSOR_HPP
 
-class		Sensor
-{
-	public:
-		
-	private:
+class Sensor {
+public:
+private:
 };
 
 #endif
--- Libraries/SensorGPS/SensorGPS.hpp
+++ Libraries/SensorGPS/SensorGPS.hpp
@@ -1,41 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   SensorGPS.hpp                                      :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: sverschu <sverschu@student.codam.n>          +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2020/08/26 16:58:53 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/08/26 17:46:52 by sverschu      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SENSORGPS_HPP
-# define SENSORGPS_HPP
+#define SENSORGPS_HPP
 
 // TinyGPS documentation:
 // https://github.com/neosarchizo/TinyGPS/blob/master/examples/test_with_gps_device/test_with_gps_device.ino
 
 #include <SoftwareSerial.h>
 
-class					SensorGPS : public Sensor
-{
-	public:
-		void			getLocation(float *flat, float *flong);
-		void			getTime(unsigned long *age, unsigned long *date, unsigned long *time);
-		void			getSpeed(float *speed);
-		void			getCourse(float *course);
-		void			update();
-						SensorGPS(const int rxPin, const int txPin);
-						~SensorGPS;
-	private:
-		SoftwareSerial	ss;
-		TinyGPS			gps;
-		float			flat, flon, kmph;
-		unsigned long	age, date, time, chars = 0;
-		unsigned short	sentences = 0, checksum = 0;
-		int				year;
-		byte			month, day, hour, minute, second, hundredths;
+class SensorGPS : public Sensor {
+public:
+    void getLocation(float* flat, float* flong);
+    void getTime(unsigned long* age, unsigned long* date, unsigned long* time);
+    void getSpeed(float* speed);
+    void getCourse(float* course);
+    void update();
+    SensorGPS(const int rxPin, const int txPin);
+    ~SensorGPS;
+
+private:
+    SoftwareSerial ss;
+    TinyGPS gps;
+    float flat, flon, kmph;
+    unsigned long age, date, time, chars = 0;
+    unsigned short sentences = 0, checksum = 0;
+    int year;
+    byte month, day, hour, minute, second, hundredths;
 };
 
 #endif
