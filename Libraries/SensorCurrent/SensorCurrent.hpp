e12d7242711d835fa49825eeedcaa5a1c143d621
thimovandermeer <thimovandermeer@gmail.com>
2020-09-02 17:36:24 +0200

    Current sensor working only no error control

--- Libraries/SensorCurrent/SensorCurrent.hpp
+++ Libraries/SensorCurrent/SensorCurrent.hpp
@@ -1,9 +1,21 @@
 #ifndef SENSORCURRENT_HPP
 #define SENSORCURRENT_HPP
 
+#include <arduino.h>
+
 class SensorCurrent {
 public:
+	float	getCurrent();
+	bool	update();
+	SensorCurrent(const int _analogPin);
+	~SensorCurrent();
 private:
+	int		analogPin;
+	long 	readVref();
+	float	readDCCurrent(); // idem bovenstaande
+	float 	Vref  = 0;
+	const 	int mVperAmp = 100; // use 185 for 5A Module, and 66 for 30A Module
+	float 	DCCurrent;
 };
 
 #endif
