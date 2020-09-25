0d09953459d27cb5466e50df8a4f4a743f7e081c
s-t-a-n <stan@wopwop.cc>
2020-09-25 19:12:22 +0200

    fixed naming and styling of MagneticSensorLSM303, SensorUltrasonic. removed dependency SensorCompass since it is broken

--- Libraries/SensorUltrasonic/SensorUltrasonic.cpp
+++ Libraries/SensorUltrasonic/SensorUltrasonic.cpp
@@ -1,26 +1,30 @@
 #include "SensorUltrasonic.hpp"
 
 SensorUltrasonic::SensorUltrasonic(const int pin)
-	: _analogPin(pin) 
-{ 
-	this->update();
+    : _analogPin(pin)
+{
+    this->update();
 }
 
 SensorUltrasonic::~SensorUltrasonic() { }
 
-bool SensorUltrasonic::update() {
-	
-	_sensity_t = analogRead(_analogPin);
+bool SensorUltrasonic::update()
+{
 
-	_dist_t = _sensity_t * MAX_RANGE / ADC_SOLUTION;
+    _sensity_t = analogRead(_analogPin);
 
-	/*
+    _dist_t = _sensity_t * SON_MAX_RANGE / ADC_RESOLUTION;
+
+    /*
 	**	No error handling implemented yet (if at all necessary).
+	*** RE: definitely necessary: take average and store last average value
+	*** to filter out wrong readings.
 	*/
 
-	return (true);
+    return (true);
 }
 
-float SensorUltrasonic::getDistance() {
-	return (_dist_t);
+float SensorUltrasonic::getDistance()
+{
+    return (_dist_t);
 }
--- Libraries/SensorUltrasonic/SensorUltrasonic.hpp
+++ Libraries/SensorUltrasonic/SensorUltrasonic.hpp
@@ -6,30 +6,31 @@
 /*!
 **	@brief The max measurement value of the module is 520cm.
 */
-#define	MAX_RANGE		(520)
+#define SON_MAX_RANGE (520)
 
 /*!
 **	@brief ADC accuracy of Arduino (set default for Arduino UNO -> 10 bit).
+*** RE: if this is a global characteristic of an Arduino, this should be in a global file.
 */
-#define ADC_SOLUTION	(1023.0)
+#define ADC_RESOLUTION (1023.0)
 
 class SensorUltrasonic {
 public:
-	SensorUltrasonic(const int pin);
-	~SensorUltrasonic();
+    SensorUltrasonic(const int pin);
+    ~SensorUltrasonic();
 
-	bool update();
+    bool update();
 
-	/*!
+    /*!
 	**	@brief Gets calculated distance from sensor.
 	**	@return Floating point calculated distance in centimeters.
 	*/
-	float getDistance();
+    float getDistance();
 
 private:
-	int _analogPin;
-	float _dist_t;
-	float _sensity_t;
+    int _analogPin;
+    float _dist_t;
+    float _sensity_t;
 };
 
 #endif
