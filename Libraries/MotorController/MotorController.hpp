d2523482d71f206c9738404a589cf077815aa848
Stan Verschuuren <sverschu@student.codam.nl>
2020-10-20 22:06:39 +0200

    cleaned motorcontroller -> working in branch 'motorcontroller'

--- Libraries/MotorController/MotorController.hpp
+++ Libraries/MotorController/MotorController.hpp
@@ -1,21 +1,9 @@
 #ifndef MOTORCONTROLLER_HPP
 #define MOTORCONTROLLER_HPP
-#include <Arduino.h>
 
 class MotorController {
 	private:
-		const char	_encoder0pinA; //A pin -> the interrupt pin 0
-		const char	_encoder0pinB; //B pin -> the digital pin 4
-		char		_encoder0PinALast;
-		int			_duration; //the number of the pulses
-		bool		_direction; //the rotation direction
-
 	public:
-		MotorController();
-		~MotorController();
-		
-		void	set_direction(bool new_direction);
-		void 	wheelSpeed();
 }
 
 #endif
