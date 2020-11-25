9ab794ebd90f1286831eecabb6747ad4dc629156
s-t-a-n <stan@wopwop.cc>
2020-11-25 18:43:05 +0100

    pinlout added to pins.h

--- LLC1/pins.h
+++ LLC1/pins.h
@@ -2,3 +2,45 @@
 *	Pinlayout is defined here.
 *	-- layout for Atmega2560/Mega
 */
+
+#define MOTOR_FRONT_LEFT_PWM	D4
+#define MOTOR_FRONT_LEFT_A		D23
+#define MOTOR_FRONT_LEFT_B		D25
+
+#define MOTOR_FRONT_RIGHT_PWM	D5
+#define MOTOR_FRONT_RIGHT_A		D27
+#define MOTOR_FRONT_RIGHT_B		D29
+
+#define MOTOR_BACK_LEFT_PWM		D6
+#define MOTOR_BACK_LEFT_A		D31
+#define MOTOR_BACK_LEFT_B		D33
+
+#define MOTOR_BACK_RIGHT_PWM	D7
+#define MOTOR_BACK_RIGHT_A		D35
+#define MOTOR_BACK_RIGHT_B		D37
+
+#define HALL_FRONT_LEFT			D18
+#define HALL_FRONT_RIGHT		D19
+#define HALL_BACK_LEFT			D2
+#define HALL_BACK_RIGHT			D3
+
+#define IMU_SDA					D20
+#define IMU_SCL					D21
+
+#define GPS_TX					D16
+#define GPS_RX					D17
+
+#define CURRENT_FRONT_LEFT		A0
+#define CURRENT_FRONT_RIGHT		A1
+#define CURRENT_BACK_LEFT		A2
+#define CURRENT_BACK_RIGHT		A3
+
+#define SONAR_FRONT_LEFT		A4
+#define SONAR_FRONT_RIGHT		A5
+#define SONAR_BACK_LEFT			A6
+#define SONAR_BACK_RIGHT		A7
+
+#define RELAY_POWER				D48
+#define RELAY_FAN				D46
+#define RELAY_V12				D44
+#define RELAY_MOTORS			D42
