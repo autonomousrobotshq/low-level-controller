f4e34ac631b61feeca0dd5c63d7ca96e8d031da5
Stan Verschuuren <sverschu@student.codam.nl>
2020-11-28 22:43:58 +0100

    cleaned up main

--- LLC1/llc1.cpp
+++ LLC1/llc1.cpp
@@ -1,50 +1,20 @@
 #include "llc1.hpp"
 
 /*
-** SAMPLE CODE (for now) : do not upload without verifying libraries and ports!
+** SAMPLE CODE
 */
 
-// this is the global time variable which every class instance may use to
-// perform time based logic.
-static unsigned long globMillis;
+use namespace sandbox;
 
-// example of how to use ros publish/subscribe
-ros::NodeHandle node_handle;
-
-std_msgs::String pub_msg; 
-std_msgs::UInt16 sub_msg;
-
-void subscriberCallback(const std_msgs::UInt16& sub_msg) {
-  if (sub_msg.data == 1) {
-	//react to event
-  } else {
-	//do something else
-  }
-}
-
-ros::Publisher						publisher("pub_name", &pub_msg);
-ros::Subscriber<std_msgs::UInt16>	subscriber("sub_name", &subscriberCallback);
+Sandbox sandbox;
 
 int main(void)
 {
-	node_handle.initNode();
-  	node_handle.advertise(publisher);
-  	node_handle.subscribe(subscriber);
 
-	// example loop
-	while (1)
+	sandbox.Setup();
+	while (true)
 	{
-		if (HIGH == HIGH) {
-    		pub_msg.data = "HIGH";
-  		} else {
-    		pub_msg.data = "LOW";
-  		}
- 		publisher.publish( &pub_msg );
-  		node_handle.spinOnce();
+		// all your code here
+		sandbox.SpinOnce();
 	}
-
-    SensorGPS sg(3, 4);
-	SensorHall(15, 16, 0, 16, &globMillis);
-    SensorCurrent(5);
-    SensorTemp(8);
 }
