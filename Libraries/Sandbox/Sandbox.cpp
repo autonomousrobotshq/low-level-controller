7e67d9a05e1905248d35522e63f27d74de03436b
Stan Verschuuren <sverschu@student.codam.nl>
2020-11-28 22:45:30 +0100

    started on sandbox

--- Libraries/Sandbox/Sandbox.cpp
+++ Libraries/Sandbox/Sandbox.cpp
@@ -1,4 +1,38 @@
+#include "Sandbox/Sandbox.hpp"
 
-#include "Sandbox.hpp"
+namespace sandbox {
 
+Sandbox::Sandbox()
+{
+	if (sb)
+	{
+		// crit: "Second initialisation of Sandbox!"
+	}
+	else
+	{
+		sb = this;
+	}
 
+}
+
+Sandbox::~Sandbox()
+{
+
+}
+
+void	Sandbox::Setup()
+{
+
+}
+
+void	Sandbox::SpinOnce()
+{
+
+}
+
+void	Driver(e_corner corner, e_drive_action action)
+{
+
+}
+
+} //namespace sandbox
--- Libraries/Sandbox/Sandbox.hpp
+++ Libraries/Sandbox/Sandbox.hpp
@@ -3,10 +3,47 @@
 #ifndef SANDBOX_HPP
 # define SANDBOX_HPP
 
+#include "Controllers/Motor.hpp"
+#include "Sensors/IMU.hpp"
+#include "Sensors/GPS.hpp"
+#include "Sensors/Temp.hpp"
+#include "Sensors/Ultrasonic.hpp"
+
 namespace sandbox {
 
+class Sandbox
+{
+	public:
+		void	Setup();
+		void	SpinOnce();
+		Sandbox();
+		~Sandbox();
+	private:
+		unsigned int		_glob_millis;
+		ControllerMotor 	_controller_motor;
+		SensorIMU			_sensor_imu;
+		SensorGPS			_sensor_gps;
+		SensorUltrasonic	_sensor_ultrasonic;
+		SensorTemp			_sensor_temp;
+};
+
+static Sandbox *sb;
+
+// public functions
+void	Driver(e_corner corner, e_drive_action action);
+
+int		IMUGetNavigationAngle();
+Vec3	IMUGetMagnetoData();
+Vec3	IMUGetAcceleroData();
+
+int		USGetDistance(e_corner corner;
+
+void	GPSGetLocation(float *flat, float *flon);
+void	GPSGetTime(unsigned long *age, unsigned long *date, unsigned long *time); // overkill?
+int		GPSGetSpeed();
+int		GPSGetCourse();
+
+int		TEMPGetTemp();
 
 } // namespace sandbox
-
-
 #endif
