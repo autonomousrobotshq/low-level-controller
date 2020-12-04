931ba227120e8fc67e6b052e52a109deba6a691d
thimovandermeer <thimovandermeer@gmail.com>
2020-12-04 11:45:20 +0100

    Changed the correction handling

--- Libraries/Logic/Steering.cpp
+++ Libraries/Logic/Steering.cpp
@@ -2,7 +2,7 @@
 #include "Common/Deployment.hpp"
 #include "Sandbox/Sandbox.hpp"
 #include <math.h>
-
+#define  PRECISION 5
 using namespace sb;
 
 LogicSteering::LogicSteering(Sandbox& sandbox)
@@ -17,42 +17,54 @@ LogicSteering::~LogicSteering()
 void LogicSteering::_turn(int angle)
 {
     int power = 10; // need to do this differently
-    if (angle < 0)
-        power *= -1;
-    float correction = -5.0;
-    if (angle > 0)
-        angle += correction;
-    else if (angle < 0)
-        angle -= correction;
-
-    int rpmcount = 0;
+    int revelationcount = 0;
 
     float distance = abs(angle) / 360.0 * _pivotCirc;
 
     int numRev = distance / _wheelCirc;
 
     int targetCount = numRev * _countsPerRev;
-    _pivot(angle, rpmcount, targetCount, power); // we can test this type of turning
+    _pivot(angle, revelationcount, targetCount, power); // we can test this type of turning
     _turnAngle(angle, targetCount, power); // or we can use this one;
 }
 
-void LogicSteering::_pivot(int angle, int rpmcount, int targetCount, int power)
+void LogicSteering::_VerifyDirection(int expected_angle)
 {
+	int current_angle;
+	int adjusted_angle;
+	current_angle = _sandbox.GPSGetCourse();
+	if (current_angle + PRECISION < expected_angle || current_angle - PRECISION > expected_angle)
+	{
+		// debugging message is needed here because we did not turn as much as we needed
+		adjusted_angle = expected_angle - current_angle;
+		_retryCount += 1;
+		if (_retryCount < 10)
+		{
+			_pivot(adjusted_angle); // choose one of these
+			_turnAngle(adjusted_angle); // choose one of these
+			// protection against infinite loop
+		}
+		else
+			;// error message not reached target angle within 10 tries.
+	}
+}
 
+void LogicSteering::_pivot(int angle, int revelationcount, int targetCount, int power)
+{
     if (angle < 0) {
         _sandbox.Driver(FRONT_LEFT, FORWARD, power);
         _sandbox.Driver(BACK_LEFT, FORWARD, power);
         _sandbox.Driver(FRONT_RIGHT, BACKWARD, power);
         _sandbox.Driver(BACK_RIGHT, BACKWARD, power);
-        while (abs(rpmcount < targetCount)) {
-            if (_sandbox.GetRPM(FRONT_LEFT) != _sandbox.GetRPM(BACK_RIGHT)) {
+        while (abs(revelationcount < targetCount)) {
+            if (_sandbox.GetRevelation(FRONT_LEFT) != _sandbox.GetRevelation(BACK_RIGHT)) {
                 _sandbox.Driver(FRONT_LEFT, HALT, power);
                 _sandbox.Driver(BACK_LEFT, HALT, power);
                 _sandbox.Driver(FRONT_RIGHT, HALT, power);
                 _sandbox.Driver(BACK_RIGHT, HALT, power);
                 // should be changed why do we need power if we are halting?
             }
-            rpmcount += _sandbox.GetRPM(FRONT_LEFT); // I added this so someone has to check if this works
+            revelationcount += _sandbox.GetRevelation(FRONT_LEFT); // I added this so someone has to check if this works
         }
         _sandbox.Driver(FRONT_LEFT, HALT, power);
         _sandbox.Driver(BACK_LEFT, HALT, power);
@@ -64,21 +76,22 @@ void LogicSteering::_pivot(int angle, int rpmcount, int targetCount, int power)
         _sandbox.Driver(BACK_LEFT, BACKWARD, power);
         _sandbox.Driver(FRONT_RIGHT, FORWARD, power);
         _sandbox.Driver(BACK_RIGHT, FORWARD, power);
-        while (abs(rpmcount < targetCount)) {
-            if (_sandbox.GetRPM(FRONT_LEFT) != _sandbox.GetRPM(BACK_RIGHT)) {
+        while (abs(revelationcount < targetCount)) {
+            if (_sandbox.GetRevelation(FRONT_LEFT) != _sandbox.GetRevelation(BACK_RIGHT)) {
                 _sandbox.Driver(FRONT_LEFT, HALT, power);
                 _sandbox.Driver(BACK_LEFT, HALT, power);
                 _sandbox.Driver(FRONT_RIGHT, HALT, power);
                 _sandbox.Driver(BACK_RIGHT, HALT, power);
                 // should be changed why do we need power if we are halting?
             }
-            rpmcount += _sandbox.GetRPM(FRONT_LEFT); // I added this so someone has to check if this works
+            revelationcount += _sandbox.GetRevelation(FRONT_LEFT); // I added this so someone has to check if this works
         }
         _sandbox.Driver(FRONT_LEFT, HALT, power);
         _sandbox.Driver(BACK_LEFT, HALT, power);
         _sandbox.Driver(FRONT_RIGHT, HALT, power);
         _sandbox.Driver(BACK_RIGHT, HALT, power);
     }
+	_VerifyDirection(angle, PRECISION);
 }
 
 void LogicSteering::_turnAngle(int angle, int targetCount, int power)
@@ -90,7 +103,7 @@ void LogicSteering::_turnAngle(int angle, int targetCount, int power)
         _sandbox.Driver(BACK_LEFT, FORWARD, power);
         long leftcount = 0;
         while (leftcount < targetCount) {
-            leftcount += _sandbox.GetRPM(FRONT_LEFT);
+            leftcount += _sandbox.GetRevelation(FRONT_LEFT);
         }
     } else {
         _sandbox.Driver(FRONT_RIGHT, FORWARD, power);
@@ -99,13 +112,14 @@ void LogicSteering::_turnAngle(int angle, int targetCount, int power)
         _sandbox.Driver(BACK_LEFT, HALT, power);
         long rightcount = 0;
         while (rightcount < targetCount) {
-            rightcount += _sandbox.GetRPM(BACK_RIGHT);
+            rightcount += _sandbox.GetRevelation(BACK_RIGHT);
         }
     }
     _sandbox.Driver(FRONT_RIGHT, HALT, power);
     _sandbox.Driver(BACK_RIGHT, HALT, power);
     _sandbox.Driver(FRONT_LEFT, HALT, power);
     _sandbox.Driver(BACK_LEFT, HALT, power);
+	_VerifyDirection(angle);
 }
 
 void LogicSteering::_driveDistance(int distance)
@@ -125,8 +139,8 @@ void LogicSteering::_driveDistance(int distance)
     float targetCount = numRev * _countsPerRev;
 
     while (abs(rightcount) < abs(targetCount)) {
-        leftCount = _sandbox.GetRPM(FRONT_LEFT);
-        rightcount = _sandbox.GetRPM(BACK_RIGHT);
+        leftCount = _sandbox.GetRevelation(FRONT_LEFT);
+        rightcount = _sandbox.GetRevelation(BACK_RIGHT);
 
         leftDiff = abs(leftCount - prevLeftCount);
         rightDiff = abs(rightcount - prevRightCount);
@@ -154,6 +168,7 @@ void LogicSteering::_driveDistance(int distance)
 
 void LogicSteering::driveLogic(int distance, int angle)
 {
+	_starting_angle = _sandbox.GPSGetCourse();
     if (angle != 0)
         _turn(angle);
     // drive straight the distance given by the ROS node
--- Libraries/Logic/Steering.hpp
+++ Libraries/Logic/Steering.hpp
@@ -1,7 +1,6 @@
 #ifndef LOGIC_STEERING_HPP
 #define LOGIC_STEERING_HPP
 #include "Sandbox/Sandbox.hpp"
-
 using namespace sb;
 
 class LogicSteering {
@@ -11,6 +10,7 @@ public:
     void driveLogic(int distance, int angle);
 
 private:
+	void _VerifyDirection(int angle);
     void _turn(int angle);
     void _pivot(int angle, int rpmcount, int targetCount, int power);
     void _turnAngle(int angle, int targetCount, int power);
@@ -18,8 +18,10 @@ private:
 
 private: // data members
     Sandbox& _sandbox;
-    const int _countsPerRev = 64;
-    const int _wheelDiam = 120;
+	int			_retryCount;
+	int 		_starting_angle; 
+    const int 	_countsPerRev = 64;
+    const int 	_wheelDiam = 120;
     const float _wheelCirc = 376.99;
     const float _pivotDiam = 203.763;
     const float _pivotCirc = 640.140;
