f3b58e9e1dbf282915f77333f35a3055992ee4be
s-t-a-n <stan@wopwop.cc>
2020-12-04 12:19:42 +0100

    Added test for Motorcontroller

---
+++ LLC1/Tests/MotorController.cpp
@@ -0,0 +1,37 @@
+#include "Common/Deployment.hpp"
+#include "Sandbox/Sandbox.hpp"
+#include "Sensors/IMU.hpp"
+#include "Common/Platform.hpp"
+#include "Logic/Steering.hpp"
+
+/*
+** SAMPLE CODE
+*/
+
+using namespace sb;
+
+Sandbox *sandbox;
+
+void setup() //runs on startup
+{
+	sandbox = new Sandbox();
+	sandbox->Setup();
+
+	Serial.begin(115200);
+
+	Serial.println("Test_MotorController.cpp");
+}
+
+void loop() // loops indefinitely
+{
+
+	Driver(ALL, FORWARD);
+
+	delay (20);
+	sandbox->SpinOnce();
+}
+
+void post() // runs after break in loop()
+{
+	Serial.println("DIDDA");
+}
