c4d25de541f305573b6e845b06e3c5c5f1a5b06c
Flint Louis <flintlouis@Flints-MacBook-Pro.local>
2020-12-04 13:44:50 +0100

    tinkering with pivot nothing serious

--- Libraries/Logic/Steering.cpp
+++ Libraries/Logic/Steering.cpp
@@ -14,12 +14,45 @@ LogicSteering::~LogicSteering()
 {
 }
 
-
-void LogicSteering::Drive(uint8_t distance, uint8_t throttle)
+void LogicSteering::Drive(float flat, float flon, uint8_t throttle)
 {
-
+    (void)flat;
+    (void)flon;
+    (void)throttle;
 }
-void LogicSteering::Pivot(e_turn turn)
+
+// Pivot robot towards navigation angle
+void LogicSteering::Set_Pivot(e_turn turn, int nav_angle)
 {
-    
+    this->_turn = turn;
+    this->_nav_angle = nav_angle;
+    this->_pivot = true;
+}
+
+void LogicSteering::Pivot()
+{
+    if (!this->_pivot)
+        return ;
+    int current_angle = this->_sandbox.IMUGetNavigationAngle();
+    if (current_angle >= this->_nav_angle - PIVOT_RANGE or current_angle <= this->_nav_angle + PIVOT_RANGE) {
+        this->_sandbox.Driver(ALL, HALT);
+        this->_pivot = false;
+        return ;
+    }
+    switch (this->_turn) {
+        case LEFT:
+            this->_sandbox.Driver(FRONT_LEFT, BACKWARD, 50);
+            this->_sandbox.Driver(BACK_LEFT, BACKWARD, 50);
+            this->_sandbox.Driver(FRONT_RIGHT, FORWARD, 50);
+            this->_sandbox.Driver(BACK_RIGHT, FORWARD, 50);
+            break ;
+        case RIGHT:
+            this->_sandbox.Driver(FRONT_LEFT, FORWARD, 50);
+            this->_sandbox.Driver(BACK_LEFT, FORWARD, 50);
+            this->_sandbox.Driver(FRONT_RIGHT, BACKWARD, 50);
+            this->_sandbox.Driver(BACK_RIGHT, BACKWARD, 50);
+            break ;
+        default:
+            break ;
+    }
 }
--- Libraries/Logic/Steering.hpp
+++ Libraries/Logic/Steering.hpp
@@ -4,6 +4,8 @@
 
 using namespace sb;
 
+#define PIVOT_RANGE 5
+
 enum e_turn {
     LEFT,
     RIGHT
@@ -13,11 +15,15 @@ class LogicSteering {
 public:
     LogicSteering(Sandbox& sandbox);
     ~LogicSteering();
-    Drive(uint8_t distance, uint8_t throttle);
-    Pivot(e_turn turn);
+    void Drive(float flat, float flon, uint8_t throttle);
+    void Pivot();
+    void Set_Pivot(e_turn turn, int degr);
 
 private:
     Sandbox& _sandbox;
+    e_turn _turn;
+    int _nav_angle;
+    bool _pivot;
 };
 
 #endif
