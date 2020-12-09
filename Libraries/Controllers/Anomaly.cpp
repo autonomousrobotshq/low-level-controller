bf33627708c7532ffab5491401e0258ce678a3e9
s-t-a-n <stan@wopwop.cc>
2020-12-09 20:57:40 +0100

    updated Controllers/Anomaly to reflect new State.hpp changes

--- Libraries/Controllers/Anomaly.cpp
+++ Libraries/Controllers/Anomaly.cpp
@@ -1,22 +1,5 @@
 #include "Controllers/Anomaly.hpp"
 
-ControllerProximity::ControllerProximity()
-{
-    for (int i = 0; i < NUM_ULTRASONIC; i++)
-        _ultrasonic_sensors[i] = new SensorUltrasonic(LLC::pins_ultrasonic[i]);
-}
-
-ControllerProximity::~ControllerProximity()
-{
-    for (int i = 0; i < NUM_ULTRASONIC; i++)
-        delete _ultrasonic_sensors[i];
-}
-
-int ControllerProximity::GetDistance(const e_corner corner)
-{
-    return (_ultrasonic_sensors[corner]->GetDistance());
-}
-
 ControllerAnomaly::ControllerAnomaly(ControllerLifetime* controller_lifetime)
     : _controller_lifetime(controller_lifetime)
 {
@@ -24,36 +7,11 @@ ControllerAnomaly::ControllerAnomaly(ControllerLifetime* controller_lifetime)
 
 ControllerAnomaly::~ControllerAnomaly() { }
 
-bool ControllerAnomaly::HandleErrno(uint8_t errno)
+bool ControllerAnomaly::HandleError(const e_state state)
 {
-    switch (errno) {
-    case OVERHEATING_ERRNO:
-        this->_controller_lifetime->Lifephase(COOLDOWN);
-        // Handle errno
-        break;
-    case HEATWARNING_ERRNO:
-        // Handle errno
-        break;
-    case US_PROXIMITY_ERRNO:
-        // Handle errno
-        break;
-    case LOW_BATTERY_ERRNO:
-        // Handle errno
-        break;
-    case OVER_CURRENT_ERRNO:
-        // Handle errno
-        break;
-    case MOTOR_FAILURE_ERRNO:
-        // Handle errno
-        break;
-#if VERBOSITY & DEBUG
-    case LOW_RAM_ERRNO:
-        // Send debug message
-        break;
-#endif
+    switch (state) {
     default:
-        return false;
-        break;
+        return (false);
     }
-    return true;
+    return (true);
 }
--- Libraries/Controllers/Anomaly.hpp
+++ Libraries/Controllers/Anomaly.hpp
@@ -2,29 +2,16 @@
 #define CONTROLLER_PROXIMITY_HPP
 
 #include "Common/Datatypes.hpp"
-#include "Common/Errno.hpp"
-#include "Common/Platform.hpp"
+#include "Common/State.hpp"
 #include "Controllers/Controller.hpp"
 #include "Controllers/Lifetime.hpp"
-#include "Sensors/Ultrasonic.hpp"
-
-// should the MotorController class hold ownership over the ultrasonic sensor?
-class ControllerProximity : public Controller {
-public:
-    ControllerProximity();
-    ~ControllerProximity();
-    int GetDistance(const e_corner corner);
-
-private:
-    SensorUltrasonic* _ultrasonic_sensors[NUM_ULTRASONIC];
-};
 
 class ControllerAnomaly {
 public:
     ControllerAnomaly(ControllerLifetime* controller_lifetime);
     ~ControllerAnomaly();
 
-    bool HandleErrno(uint8_t errno);
+    bool HandleError(const e_state state);
 
 private:
     ControllerLifetime* _controller_lifetime;
