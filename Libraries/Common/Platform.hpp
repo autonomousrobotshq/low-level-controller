cb5c833755675ce3d087262d07dadcac4cf8a283
s-t-a-n <stan@wopwop.cc>
2020-12-09 20:57:04 +0100

    cleaned up Datatypes.hpp and Platform.hpp, added e_siglevel, reflected changes to new State.hpp

--- Libraries/Common/Platform.hpp
+++ Libraries/Common/Platform.hpp
@@ -12,14 +12,11 @@
 #include "Common/Datatypes.hpp"
 #include "Common/Deployment.hpp"
 
-/* ANOMALY DATA */
-#define ULTRASONIC_DIST 30
-#define OVERHEATING 70
-#define HEAT_WARNING 50
-#define LOW_BATTERY 15
-#define RAM_SPACE 100
-#define OVER_CURRENT 30
-#define UNDER_CURRENT 10 ////////?????
+/*
+ * LL CONTROLLER SPECIFIC CONFIGURATIONS
+ * Don't use 'LLC1 or LLC2 etc. -> LLC is declared in Deployment.hpp
+*/
+namespace LLC1 {
 
 /* RANGES */
 #define MOTOR_THROTTLE_HIGH 100
@@ -31,11 +28,6 @@
 #define NUM_RELAYS 6
 #define NUM_TEMP 1
 
-/* PORTS */
-
-// Don't use 'LLC1 or LLC2 etc. -> LLC is declared in Deployment.hpp
-namespace LLC1 {
-
 /* IMU CALIBRATION */
 
 // X_MIN, X_MAX, Y_MIN, Y_MAX, Z_MIN, Z_MAX
@@ -46,6 +38,14 @@ const t_imu_calibration imu_calibration_accelerometer = { -32767, 32767,
     -32767, 32767,
     -32767, 32767 };
 #define IMU_SAMPLE_COUNT 3
+
+enum e_relays {
+    REL_POWER = 0,
+    REL_V12 = 2,
+    REL_MOTORS = 3,
+    REL_FAN = 4,
+    REL_BEEP = 6
+};
 
 // PWM, A, B
 const t_pins_motor pins_motors[NUM_MOTORS] = {
@@ -92,59 +92,12 @@ const uint8_t pins_relay[NUM_RELAYS] = {
     44
 };
 
+// BEEPER
+const t_pins_physicalfeedback pins_physicalfeedback = { 46 };
+
 const uint8_t pins_temp[NUM_TEMP] {
     0 // THESE PINS NEEDS TO BE RESEARCHED
 };
 } // namespace LLC1
-
-// THE OLD WAY
-//#define RELAY_POWER				48	// DIGITAL
-//#define RELAY_FAN				46	// DIGITAL
-//#define RELAY_V12				44	// DIGITAL
-//#define RELAY_MOTORS			42	// DIGITAL
-//
-//
-//#define RELAY_POWER				48	// DIGITAL
-//#define RELAY_FAN				46	// DIGITAL
-//#define RELAY_V12				44	// DIGITAL
-//#define RELAY_MOTORS			42	// DIGITAL
-//
-////ready
-//#define MOTOR_FRONT_LEFT_PWM	4	// DIGITAL
-//#define MOTOR_FRONT_LEFT_A		23	// DIGITAL
-//#define MOTOR_FRONT_LEFT_B		25	// DIGITAL
-//
-//#define MOTOR_FRONT_RIGHT_PWM	5	// DIGITAL
-//#define MOTOR_FRONT_RIGHT_A		27	// DIGITAL
-//#define MOTOR_FRONT_RIGHT_B		29	// DIGITAL
-//
-//#define MOTOR_BACK_LEFT_PWM		6	// DIGITAL
-//#define MOTOR_BACK_LEFT_A		31	// DIGITAL
-//#define MOTOR_BACK_LEFT_B		33	// DIGITAL
-//
-//#define MOTOR_BACK_RIGHT_PWM	7	// DIGITAL
-//#define MOTOR_BACK_RIGHT_A		35	// DIGITAL
-//#define MOTOR_BACK_RIGHT_B		37	// DIGITAL
-//
-//#define HALL_FRONT_LEFT			18	// DIGITAL
-//#define HALL_FRONT_RIGHT		19	// DIGITAL
-//#define HALL_BACK_LEFT			2	// DIGITAL
-//#define HALL_BACK_RIGHT			3	// DIGITAL
-//
-//#define IMU_SDA					20	// DIGITAL
-//#define IMU_SCL					21	// DIGITAL
-//
-//#define GPS_TX					16	// DIGITAL
-//#define GPS_RX					17	// DIGITAL
-//
-//#define CURRENT_FRONT_LEFT		0	// ANALOG
-//#define CURRENT_FRONT_RIGHT		1	// ANALOG
-//#define CURRENT_BACK_LEFT		2	// ANALOG
-//#define CURRENT_BACK_RIGHT		3	// ANALOG
-//
-//#define SONAR_FRONT_LEFT		4	// ANALOG
-//#define SONAR_FRONT_RIGHT		5	// ANALOG
-//#define SONAR_BACK_LEFT			6	// ANALOG
-//#define SONAR_BACK_RIGHT		7	// ANALOG
 
 #endif
