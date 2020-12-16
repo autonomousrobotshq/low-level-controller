f73aa437cefb011c061fab3ede34a3b62f7f57cc
s-t-a-n <stan@wopwop.cc>
2020-12-16 15:41:30 +0100

    changed enums e_side/e_corner to enum class types

--- Libraries/Common/Datatypes.hpp
+++ Libraries/Common/Datatypes.hpp
@@ -8,24 +8,26 @@
 #include <HardwareSerial.h>
 #include <stdint.h>
 
-enum e_side {
-    LEFT_SIDE = 0,
-    RIGHT_SIDE = 1,
-    BOTH_SIDES = 2
+enum class e_side {
+    LEFT,
+    RIGHT,
+	FRONT,
+	BACK,
+	ALL
 };
 
-enum e_corner {
-    FRONT_LEFT = 0,
-    FRONT_RIGHT = 1,
-    BACK_LEFT = 2,
-    BACK_RIGHT = 3,
-    ALL = 4
+enum class e_corner {
+    FRONT_LEFT,
+    FRONT_RIGHT,
+    BACK_LEFT,
+    BACK_RIGHT,
+    ALL
 };
 
-enum e_siglevel {
-    SIG_INFO,
-    SIG_WARN,
-    SIG_CRIT
+enum class e_siglevel {
+    INFO,
+    WARN,
+    CRIT
 };
 
 typedef struct s_exec_intervals {
